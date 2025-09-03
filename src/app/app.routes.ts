7654d5d89023876f41fc5cd4bcaa8e10e93295f1
nghiemchibao01 <nghiemchibao01@gmail.com>
2025-09-03 20:55:19 +0700

    Update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Friends } from './pages/friends/friends';
+import { FriendDetail } from './pages/friend-detail/friend-detail';
+import { About } from './pages/about/about';
+import { NotFound } from './pages/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Home},
+    {path: 'friends', component: Friends},
+    {path: 'friend/:id', component: FriendDetail},
+    {path: 'about', component: About},
+    {path: '**', component: NotFound},
+];
